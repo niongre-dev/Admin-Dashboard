268ce7be43b9f5fde43532f5fda0fbf5ac92135b
Niongre-dev <dieudonneniongre6771.@gmail.com>
2024-03-18 00:27:56 +0000

    Implementation d'une partie de header

---
+++ src/components/Nav.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './nav.css';
+import NavNotice from './NavNotice';
+import NavAvatar from './NavAvatar';
+import NavMessage from './NavMessage';
+
+
+function Nav() {
+  return (
+    <nav className='header-nav ms-auto'>
+        <ul className='d-flex align-items-center'>
+           <NavNotice/> 
+           <NavMessage/>
+           <NavAvatar/>
+        </ul>
+    </nav>
+  );
+}
+
+export default Nav;
